a43d465b6af675b2d87a991df0509cf1c547f332
Austin Johnson <technicallyaustin@gmail.com>
2020-01-27 19:56:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,14 +1,14 @@
 class Student < User
   def initialize
-    knowledge = []
+    @knowledge = []
   end
   
   def learn(string_of_know)
-    knowledge << string_of_know
+    @knowledge << string_of_know
   end
   
   def knowledge 
-    knowledge
+    @knowledge
   end
 
 
